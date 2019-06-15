2774256bbcdc318b9b59164bbb22225e0d340267
Sebastian Rasmussen <sebras@gmail.com>
2019-06-15 18:17:00 +0200

    Spellcheck 01-02 "Hello, World!".

--- spellcheck.sh
+++ spellcheck.sh
@@ -9,6 +9,7 @@ pushd "$(dirname "$0")"
 spellcheck src/ch00-00-introduction.md
 spellcheck src/ch01-00-getting-started.md
 spellcheck src/ch01-01-installation.md
+spellcheck src/ch01-02-hello-world.md
 
 head -1 dict > .dict
 sed -e 1d dict | sort >> .dict
