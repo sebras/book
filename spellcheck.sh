038079c411d724d616638c033b24e5bd444d7dbc
Sebastian Rasmussen <sebras@gmail.com>
2019-08-18 04:06:37 +0200

    Spellcheck 03-03 "Functions".

--- spellcheck.sh
+++ spellcheck.sh
@@ -15,6 +15,7 @@ spellcheck src/ch02-00-guessing-game-tutorial.md
 spellcheck src/ch03-00-common-programming-concepts.md
 spellcheck src/ch03-01-variables-and-mutability.md
 spellcheck src/ch03-02-data-types.md
+spellcheck src/ch03-03-how-functions-work.md
 
 head -1 dict > .dict
 sed -e 1d dict | sort >> .dict
